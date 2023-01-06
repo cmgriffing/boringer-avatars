f0e0be327b8bc6414e047d0a18d29023f46247ae
cmgriffing <cmgriffing@users.noreply.github.com>
2023-01-06 23:37:58 +0000

    deploy: cd7a76a94603174581abce89d8422a084ffbd66b

---
+++ react/_next/static/VNBRqTVbxiZMW41GwhTO9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-27454255be72e6f1.js"],"/_error":["static/chunks/pages/_error-64ce832b4076f5a3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
