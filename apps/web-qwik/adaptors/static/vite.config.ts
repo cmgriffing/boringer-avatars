8290cde910ba703c9e53660c9972e8659d659dc7
Chris Griffing <cmgriffing@gmail.com>
2023-02-21 18:58:45 -0800

    fix: set web-qwik base in static config

--- apps/web-qwik/adaptors/static/vite.config.ts
+++ apps/web-qwik/adaptors/static/vite.config.ts
@@ -1,18 +1,19 @@
-import { staticAdaptor } from '@builder.io/qwik-city/adaptors/static/vite';
-import { extendConfig } from '@builder.io/qwik-city/vite';
-import baseConfig from '../../vite.config';
+import { staticAdaptor } from "@builder.io/qwik-city/adaptors/static/vite";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
   return {
+    base: "/boringer-avatars/qwik/",
     build: {
       ssr: true,
       rollupOptions: {
-        input: ['@qwik-city-plan'],
+        input: ["@qwik-city-plan"],
       },
     },
     plugins: [
       staticAdaptor({
-        origin: 'https://yoursite.qwik.dev',
+        origin: "https://yoursite.qwik.dev",
       }),
     ],
   };
