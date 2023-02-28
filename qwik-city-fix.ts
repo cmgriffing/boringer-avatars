2ff08c8d7337cb15459915ec03961358c9816e02
Chris Griffing <cmgriffing@gmail.com>
2023-02-27 18:09:41 -0800

    fix: make qwik-city-fix update the copied qwb-qwik instead of the original

--- qwik-city-fix.ts
+++ qwik-city-fix.ts
@@ -8,7 +8,7 @@ https://github.com/BuilderIO/qwik/issues/2462
 import fs from "fs";
 import path from "path";
 
-const indexFilePath = path.resolve(__dirname, "apps/web-qwik/dist/index.html");
+const indexFilePath = path.resolve(__dirname, "apps/docs/dist/qwik/index.html");
 
 const indexFileContents = fs.readFileSync(indexFilePath, { encoding: "utf8" });
 
