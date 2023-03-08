40fb453a0a778497d84810c24cb13a83af172357
Chris Griffing <cmgriffing@gmail.com>
2023-03-07 19:34:34 -0800

    fix: change solid to solid-js

--- packages/demo/scripts/boilerplate/solid/vite.config.ts
+++ packages/demo/scripts/boilerplate/solid/vite.config.ts
@@ -5,7 +5,7 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [
-    solidPlugin({ solid: { generate: "ssr", hydratable: true } }),
+    solidPlugin(),
     dts({
       insertTypesEntry: true,
     }),
@@ -23,7 +23,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["solid"],
+      external: ["solid-js"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
